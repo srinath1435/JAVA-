2bbe1cf4ca311176a4fbc11ff2eb2ca9760cb025
srinath1435 <perumallasrinath2003@gmail.com>
2023-11-07 10:56:34 +0530

    tst

---
+++ data types/one.java
@@ -0,0 +1,13 @@
+class A{
+    public static void main(String[] args) {
+        int main =100;
+        int b = 200;
+        float c= 4.3f;
+        double d=4.33;
+        System.out.println(main);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
---
+++ test1.java
@@ -0,0 +1,6 @@
+class A{
+    public static void main(String[]args){
+        System.out.print("Hello this is println method...");
+         System.out.print(" Hello this is println method");
+    }
+}
